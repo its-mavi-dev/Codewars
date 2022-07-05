e87130c23973d8e72c24896010c0825b957b110b
its-mavi-dev <mahaveersinghbarhath@gmail.com>
2022-07-05 14:26:51 +0530

    Beginner Series #3 Sum of Numbers

---
+++ 7kyu/Beginner-Series-3-Sum-of-Numbers.js
@@ -0,0 +1,41 @@
+/*
+
+Link: https://www.codewars.com/kata/55f2b110f61eb01779000053
+
+
+DESCRIPTION:
+
+Given two integers a and b, which can be positive or negative,
+ find the sum of all the integers between and including them and return it.
+  If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+
+FUNDAMENTALS ALGORITHMS
+
+*/
+
+// My Solution
+
+function getSum(a, b) {
+    if (a === b) return a;
+    var sum = 0;
+    for (var i = a; a > b ? i >= b : i <= b; a > b ? i-- : i++) sum += i;
+    return sum;
+}
+
+
+//Sample Tests
+
+getSum(0, -1) //-1
+getSum(0, 1) //1
